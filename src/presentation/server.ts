ae312f4658fd136f516b21e860d9a37a7e078e5f
VicDario <26585356+VicDario@users.noreply.github.com>
2024-12-19 15:12:40 -0300

    example of a spa served in express

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 
 export class Server {
 
@@ -10,7 +11,12 @@ export class Server {
     
         //* Public folder
         this.app.use(express.static('public'));
-        
+
+        this.app.get('*', (_req, res) => {
+            const indexPath = path.join(__dirname, '../../public/index.html');
+            res.sendFile(indexPath);
+        });
+
         this.app.listen(3000, () => console.log(`Server running on port 3000`))
     }
 }
