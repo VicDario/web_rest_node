9cefe2fe620fa86f3c0402dbfb7a74a6145368cb
VicDario <26585356+VicDario@users.noreply.github.com>
2024-12-26 15:02:46 -0300

    style: format

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import { Server } from "./presentation/server";
     main();
 })();
 
-function main () {
+function main() {
     const server = new Server({
         port: envs.PORT,
         publicPath: envs.PUBLIC_PATH,
