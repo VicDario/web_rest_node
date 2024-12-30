2a936aff4c4f7617a8f8919fda99916d3ef51fb3
VicDario <26585356+VicDario@users.noreply.github.com>
2024-12-29 21:32:53 -0300

    feat: crud - create

--- src/presentation/todos/controller.ts
+++ src/presentation/todos/controller.ts
@@ -1,8 +1,8 @@
 import type { Request, Response } from "express";
 
 const todos = [
-    { id: 1, text: "Buy Milk", createAt: new Date() },
-    { id: 2, text: "Buy Bread", createAt: new Date() },
+    { id: 1, text: "Buy Milk", createdAt: new Date() },
+    { id: 2, text: "Buy Bread", createdAt: null },
 ];
 
 export class TodosController {
@@ -19,9 +19,24 @@ export class TodosController {
             res.status(400).json({ error: 'ID argument is not a number' });
             return;
         }
-        
+
         const todo = todos.find((todo) => todo.id === id);
         if (todo) res.json(todo);
         else res.sendStatus(404);
     };
+
+    public createTodo = (req: Request, res: Response) => {
+        const { text } = req.body;
+        if (!text) {
+            res.status(400).json({ error: 'Text property is required'})
+            return;
+        };
+        const newTodo = {
+            id: todos.length + 1,
+            text: text as string,
+            createdAt: null,
+        };
+        todos.push(newTodo);
+        res.json(newTodo);
+    }
 }
