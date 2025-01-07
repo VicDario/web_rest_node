2413a871408f4299a49324440a1d0e79e2854620
VicDario <26585356+VicDario@users.noreply.github.com>
2025-01-07 14:25:21 -0300

    feat: add custom error

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -3,8 +3,11 @@ export * from "./dtos";
 export * from "./repositories/todo.repository";
 export * from "./entities/todo.entity";
 
+export * from "./errors/custom.error";
+
 export * from "./use-cases/todo/create-todo.usecase";
 export * from "./use-cases/todo/get-todo.usecase";
 export * from "./use-cases/todo/get-todos.usecase";
 export * from "./use-cases/todo/update-todo.usecase";
 export * from "./use-cases/todo/create-todo.usecase";
+
