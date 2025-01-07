cd952265fbc644df7104b3062ddd65d027e0bb45
VicDario <26585356+VicDario@users.noreply.github.com>
2025-01-07 13:27:33 -0300

    test: add test for update endpoint

--- tests/presentation/todos/routes.test.ts
+++ tests/presentation/todos/routes.test.ts
@@ -3,6 +3,7 @@ import { prisma } from "../../../src/data/postgres";
 import { testServer } from "../../test-server";
 import request from "supertest";
 import { text } from "stream/consumers";
+import { error } from "console";
 
 describe("Todo route testing", () => {
     beforeAll(async () => {
@@ -87,5 +88,68 @@ describe("Todo route testing", () => {
             .expect(400);
 
         expect(response.body).toEqual({ error: expect.any(String) });
+    });
+
+    test("should return an updated TODO api/todos/:id", async () => {
+        const savedTodo = await prisma.todo.create({ data: todo });
+
+        const newText = "new task text";
+        const newDate = "2023-12-20";
+
+        const response = await request(testServer.app)
+            .put(`/api/todos/${savedTodo.id}`)
+            .send({ text: newText, completedAt: newDate })
+            .expect(200);
+
+        expect(response.body).toEqual({
+            id: savedTodo.id,
+            text: newText,
+            completedAt: expect.stringContaining(newDate),
+        });
+    });
+
+    test("should return 404 if TODO not found api/todos/:id", async () => {
+        const todoId = 1;
+        const newText = "new task text";
+        const newDate = "2023-12-20";
+
+        const response = await request(testServer.app)
+            .put(`/api/todos/${todoId}`)
+            .send({ text: newText, completedAt: newDate })
+            .expect(400);
+            
+        expect(response.body).toEqual({ error: `TODO with ID ${todoId} not found` });
+    });
+
+    test("should return an updated TODO only the date api/todos/:id", async () => {
+        const savedTodo = await prisma.todo.create({ data: todo });
+        const newDate = "2023-12-20";
+
+        const response = await request(testServer.app)
+            .put(`/api/todos/${savedTodo.id}`)
+            .send({ completedAt: newDate })
+            .expect(200);
+
+        expect(response.body).toEqual({
+            id: savedTodo.id,
+            text: savedTodo.text,
+            completedAt: expect.stringContaining(newDate),
+        });
+    });
+
+    test("should return an updated TODO only the text api/todos/:id", async () => {
+        const savedTodo = await prisma.todo.create({ data: todo });
+        const newText = "Updated task text";
+
+        const response = await request(testServer.app)
+            .put(`/api/todos/${savedTodo.id}`)
+            .send({ text: newText })
+            .expect(200);
+
+        expect(response.body).toEqual({
+            id: savedTodo.id,
+            text: newText,
+            completedAt: savedTodo.completedAt?.toISOString(),
+        });
     });
 });
